10a261afcd2d8284aae196e211d66ef1f323a4eb
Juan Sebastián Urrea <js.urrea@uniandes.edu.co>
2023-11-19 01:42:04 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import { TodoCreate } from "./TodoCreate";
 
 import "./App.css";
 
-const defaultTodos = [
+const defaultItem = [
   { text: "Complete morning workout 🏋️‍♂️", completed: true },
   { text: "Write project proposal 📝", completed: false },
   { text: "Grocery shopping 🛒", completed: false },
   { text: "Learn a new recipe 👩‍🍳", completed: false },
-  { text: "Take a hike in a nearby nature reserve 🌳", completed: false },
+  { text: "Take a hike nearby 🌳", completed: false },
   { text: "Read a chapter of a book 📖", completed: false },
   { text: "Plan weekend activities 🗓️", completed: false },
   { text: "Organize workspace 🗄️", completed: false },
@@ -20,9 +20,28 @@ const defaultTodos = [
   { text: "Watch a documentary 📺", completed: false },
 ];
 
+function useLocalStorage() {
+  let parsedItem = localStorage.getItem("TaskMaster_V1");
+  parsedItem = parsedItem ? JSON.parse(parsedItem) : defaultItem;
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem("TaskMaster_V1", JSON.stringify(newItem));
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
+
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = useLocalStorage();
   const [searchValue, setSearchValue] = React.useState("");
+
+  // Sort parsedTodos such that items with completed=true come first
+  todos.sort((a, b) =>
+    a.completed === b.completed ? 0 : a.completed ? 1 : -1
+  );
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -34,7 +53,15 @@ function App() {
   const toggleTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.text === text);
-    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    if (newTodos[todoIndex].completed) {
+      newTodos.splice(todoIndex, 1);
+      newTodos.unshift(todos[todoIndex]);
+      newTodos[0].completed = false;
+    } else {
+      newTodos.splice(todoIndex, 1);
+      newTodos.push(todos[todoIndex]);
+      newTodos[todos.length - 1].completed = true;
+    }
     setTodos(newTodos);
   };
 
